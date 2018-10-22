52588622fa70888d0a690cf2c5b40a7b7df959d2
Trevor <revolution.x@gmail.com>
2018-10-22 20:49:11 +0900

    Added initial solution files

---
+++ TestScript.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquadInfoPoc
+{
+    class TestScript
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
