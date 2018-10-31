9c9f9ef72045d4e174dd77fc18cdf93ba90fa5f1
Trevor <revolution.x@gmail.com>
2018-10-31 23:12:35 +0900

    Implemented squad detection with minimal impact

--- TestScript.cs
+++ TestScript.cs
@@ -1,15 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
 
 namespace SquadInfoPoc
 {
     class TestScript
     {
+        private const string SQUAD_PROCESS_NAME = "Squad";
+        private static bool loggerRunning = false;
         static void Main(string[] args)
         {
+
+            // Create a 30 min timer 
+            var timer = new System.Timers.Timer(60000);
+
+            // Hook up the Elapsed event for the timer.
+            timer.Elapsed += IsSquadRunning;
+
+            timer.Enabled = true;
+
+            while (true)
+            {
+                Thread.Sleep(10000);
+                Console.WriteLine("Still waiting on squad");
+            }
+        }
+
+        private static void IsSquadRunning(object source, ElapsedEventArgs e)
+        {
+            bool squadRunning = Array.Exists(Process.GetProcesses(), m => m.ProcessName == SQUAD_PROCESS_NAME);
+            if (squadRunning && loggerRunning)
+            {
+                return;
+            }
+            else if (squadRunning && !loggerRunning)
+            {
+                Console.WriteLine("Starting Squad Logger Program");
+                loggerRunning = true;
+            }
+            else if (!squadRunning && loggerRunning)
+            {
+                Console.WriteLine("Stopped logger");
+                loggerRunning = false;
+            }
         }
     }
 }
